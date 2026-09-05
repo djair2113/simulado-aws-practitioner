274ca881d8f2e15d405ba29fafaba2356360465b
Djair Santos <102264704+djair2113@users.noreply.github.com>
2026-09-05 17:21:43 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,31 @@
-const CACHE_NAME = 'aws-quiz-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './questoes.json'
-];
+const CACHE_NAME = 'aws-quiz-v3';
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => {
-      return res || fetch(e.request);
-    })
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
   );
 });
