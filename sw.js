6caf544b9235c2bdd1593f89c11b2d08034c976a
Djair Santos <102264704+djair2113@users.noreply.github.com>
2026-09-08 22:54:25 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,31 +1,24 @@
-const CACHE_NAME = 'aws-quiz-v10';
+const CACHE_NAME = 'aws-quiz-v11';
 const urlsToCache = [
   './',
   './index.html',
   './questoes.json',
   './manifest.json'
-  // Adicione aqui outros arquivos estáticos do seu app (como ícones ou CSS/JS, se houver)
 ];
 
-// Instalação do Service Worker e armazenamento no cache
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
   self.skipWaiting();
 });
 
-// Ativação e limpeza de caches antigos
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
           if (cacheName !== CACHE_NAME) {
-            console.log('Removendo cache antigo:', cacheName);
             return caches.delete(cacheName);
           }
         })
@@ -35,13 +28,8 @@ self.addEventListener('activate', event => {
   self.clients.claim();
 });
 
-// Interceptação das requisições para servir o cache ou buscar na rede
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Retorna do cache se encontrar, senão busca na rede
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
